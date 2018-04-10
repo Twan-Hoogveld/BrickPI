35da119cf2a0d1b6ec0df9f0b78c120f1312c7cc
thoogveldHU <31695469+thoogveldHU@users.noreply.github.com>
2018-04-10 14:10:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,10 +37,10 @@ void goRight(int valWheel1, int valWheel2){
     BP.set_motor_power(PORT_B, valWheel2); 
 }
 
-void goForward(int valWheel1, int valWheel2){
+void goForward(){
     //Motoren gaan even snel
-    BP.set_motor_power(PORT_C, valWheel1);
-    BP.set_motor_power(PORT_B, valWheel2); 
+    BP.set_motor_power(PORT_C, 10);
+    BP.set_motor_power(PORT_B, 10); 
 }
 
 int16_t measureColor() {
