d61a2d653b96a59652592b62e9c454531d5b99cb
thoogveldHU <31695469+thoogveldHU@users.noreply.github.com>
2018-04-11 13:01:18 +0200

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -32,8 +32,8 @@ void hardLinks(){
 }
 
 void hardRechts(){
-	BP.set_motor_position_relative(PORT_C, -230);
-	BP.set_motor_position_relative(PORT_B, 230);
+	BP.set_motor_position_relative(PORT_C, -280);
+	BP.set_motor_position_relative(PORT_B, 280);
 }
 
 void hardForward(){
