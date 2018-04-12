f5e726aeec5dd50df076b241d4c08318626ef641
pepijnfaber1 <35467090+pepijnfaber1@users.noreply.github.com>
2018-04-12 16:19:36 +0200

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -89,22 +89,27 @@ void doit(){
 	}	
         if(int(measureLight()) < 2000 && int(measureColor()) > 400){
 		cout << measureLight() << " , " << measureColor() << "\n";
+		cout << "wit wit" << "\n";
 		goForward();
 		//<2000 is wit, < 400 is wit
 	}
 	else if(int(measureLight()) > 2000 && int(measureColor()) < 400){
 		cout << measureLight() << " , " << measureColor() << "\n";
+		cout << "zwart wit" << "\n";
 		goLeft(-20, 50);
 		//>2000 is zwart, < 400 is wit
 	}
 	else if(int(measureLight()) < 2000 && int(measureColor()) > 400){
 		cout << measureLight() << " , " << measureColor() << "\n";
+		cout << "wit zwart" << "\n";
 		goRight(50, -20);
+	
 		//<2000 is wit, > 400 is zwart
 	}
 	else if(int(measureLight()) > 2000 && int(measureColor()) > 400){
 		cout << measureLight() << " , " << measureColor() << "\n";
-		goRight(70, -40);
+		cout << "zwart zwart" << "\n";
+		goRight(50, -20);
 		//>2000 is zwart, >400 is zwart
 	}
 	usleep(50000);
