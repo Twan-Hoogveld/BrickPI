d08d13da5e27ecf63f07721f8037ce3050fdc99d
pepijnfaber1 <35467090+pepijnfaber1@users.noreply.github.com>
2018-04-12 12:14:43 +0200

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -1,1 +1,16 @@
+/*
+Port_1 = RGB  
+Port_2 = ULTRASONIC
+Port_3 = Kleur
+Port_B = Rechter wiel
+Port_C = Linker wiel
+*/
 
+#include "BrickPi3.h"
+#include <iostream>
+#include <signal.h>
+#include <unistd.h>
+
+using namespace std;
+
+BrickPi3 BP;
