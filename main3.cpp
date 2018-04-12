883f3f620a37204500bb62f2cdb00b0bb2308a2e
pepijnfaber1 <35467090+pepijnfaber1@users.noreply.github.com>
2018-04-12 18:17:23 +0200

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -118,12 +118,12 @@ void doit(){
 		srand (time(NULL));
 		int dice = rand()%2;
 		if (dice == 0){
-			hardLinks();
-			sleep(1);
+			goLeft(-20, 50);
+			sleep(2);
 		}
 		else if (dice == 1){
-			hardRechts();
-			sleep(1);
+			goRight(50, -20);
+			sleep(2);
 		}
 		// >2000 is zwart, <400 is zwart
 	}
